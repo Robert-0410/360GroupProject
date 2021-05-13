c3b111b413075a1da8714e8d77b35179de57b41a
Robert-0410 <62630290+Robert-0410@users.noreply.github.com>
2021-05-13 15:48:46 -0700

    Updated all JavaDocs for start up version.

--- src/view/ButtonPanel.java
+++ src/view/ButtonPanel.java
@@ -2,11 +2,20 @@ package view;
 
 import javax.swing.*;
 
+/**
+ * Singleton style class that builds the control buttons container.
+ */
 public class ButtonPanel extends JPanel {
 
     // TODO add to UML
+    /**
+     * Unique instance of the panel.
+     */
     private static ButtonPanel uniqueInstance;
 
+    /**
+     * Array for internal storage of actual arrow buttons.
+     */
     private final JButton[] myArrows= new JButton[4];
 
     /**
@@ -17,6 +26,11 @@ public class ButtonPanel extends JPanel {
     }
 
     // TODO add to UML
+
+    /**
+     * Gets unique instance of the button panel.
+     * @return only instance of the ButtonPanel.
+     */
     public static ButtonPanel getInstance() {
         if(uniqueInstance == null) {
             uniqueInstance = new ButtonPanel();
@@ -25,24 +39,43 @@ public class ButtonPanel extends JPanel {
     }
 
 
-    // getters
     // TODO: update UML for all getters for each button
+
+    /**
+     * Gets up JButton.
+     * @return up button.
+     */
     public JButton getUpButton() {
         return myArrows[0];
     }
 
+    /**
+     * Gets right JButton.
+     * @return right button.
+     */
     public JButton getRightButton() {
         return myArrows[1];
     }
 
+    /**
+     * Gets down JButton.
+     * @return down button.
+     */
     public JButton getDownButton() {
         return myArrows[2];
     }
 
+    /**
+     * Gets left JButton.
+     * @return left button.
+     */
     public JButton getLeftButton() {
         return myArrows[3];
     }
-    // buildButtonPanel() {you need all your methods that build your panel}
+
+    /**
+    Consolidation of all the sub methods that build the individual components that belong in the ButtonPanel.
+     */
     private void buildButtonPanel() {
         instantiateButtons();
         buildUpButton();
@@ -50,30 +83,47 @@ public class ButtonPanel extends JPanel {
         buildDownButton();
         buildLeftButton();
     }
+
+    /**
+    Instantiates the arrow button with the array.
+     */
     private void instantiateButtons() {
         for(int i = 0; i < myArrows.length; i++) {
             myArrows[i] = new JButton();
         }
     }
 
+    /**
+    Builds and customizes the up arrow button.
+     */
     private void buildUpButton() {
         var up = getUpButton();
         up.setText("Up");
     }
 
+    /**
+    Builds and customizes the right arrow button.
+    */
     private void buildRightButton() {
         var right = getRightButton();
         right.setText("Right");
     }
 
+    /**
+    Builds and customizes the down arrow button.
+     */
     private void buildDownButton() {
         var down = getDownButton();
         down.setText("Down");
     }
 
+    /**
+    Builds and customizes left arrow button.
+     */
     private void buildLeftButton() {
         var left = getLeftButton();
+        left.setText("Left");
     }
-    // buildArrowButtons() {handle all creation of arrows}
+
 
 }
