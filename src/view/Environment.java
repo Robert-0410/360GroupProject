423fe3123834bd05026e7cde3339fccb60a33f7d
slogansUW <83303380+slogansUW@users.noreply.github.com>
2021-05-13 20:40:15 -0700

    Updated MenuPanel - added more

--- src/view/Environment.java
+++ src/view/Environment.java
@@ -2,5 +2,43 @@ package view;
 
 import javax.swing.*;
 
-public class Environment extends JPanel {
+/**
+ * Environment for where the background will initially
+ * be displayed. After a game is selected, Environment
+ * will be used as the game playing field.
+ */
+public class Environment extends GUIPanel {
+    /**
+     * Singleton Style Unique Instance.
+     */
+    private static Environment uniqueInstance;
+
+    /**
+     * Constructor for Environment Class
+     */
+    private Environment() {
+        setUpEnvironmentPanel();
+    }
+
+    /**
+     * Get Instance Method
+     * @return uniqueInstance
+     */
+    public static Environment getInstance() {
+        if (uniqueInstance == null) {
+            uniqueInstance = new Environment();
+        }
+        return uniqueInstance;
+    }
+
+    /**
+     * Sets up the Environment panel
+     */
+    private void setUpEnvironmentPanel() {
+        setSize(750, 500);
+        JLabel environmentBackground = new JLabel(new ImageIcon("src/resources/RickAndMortyBackground.jpg"));
+        environmentBackground.setSize(750, 500);
+        add(environmentBackground);
+    }
+
 }
