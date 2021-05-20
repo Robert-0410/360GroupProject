a89c588bfd9e44bbec563684f7c69add7e6c646d
Robert-0410 <62630290+Robert-0410@users.noreply.github.com>
2021-05-20 12:14:42 -0700

    Created NewGameButtonListener.java, that will handle actions any time the New Game button is used.

---
+++ src/controller/NewGameButtonListener.java
@@ -0,0 +1,22 @@
+package controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Handles Actions related to the New Game button.
+ * @author Robert
+ * @version 1
+ */
+public class NewGameButtonListener implements ActionListener {
+
+    /**
+     * Invoked when an action occurs.
+     *
+     * @param e the event to be processed
+     */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
