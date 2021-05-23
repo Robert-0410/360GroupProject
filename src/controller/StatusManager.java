ec93c29b05200dba37c471153dafe06addcd8f96
Robert-0410 <62630290+Robert-0410@users.noreply.github.com>
2021-05-22 19:07:33 -0700

    Added more structure to the map, and changes to StatusManager. Move buttons enables now when a new game is started.

---
+++ src/controller/StatusManager.java
@@ -0,0 +1,54 @@
+package controller;
+
+import view.ButtonPanel;
+import view.MenuPanel;
+import view.QuestionPanel;
+
+
+/**
+ * Utility for status control of all game components.
+ * @author Robert
+ * @version 1
+ */
+public class StatusManager {
+
+    /**
+     * Not to be invoked.
+     */
+    private StatusManager() {}
+
+    /**
+     * Enables buttons to start playing as Rick.
+     */
+    protected static void enableGameButtons() {
+        var currentPanel = MenuPanel.getInstance();
+        // Menu Panel
+        currentPanel.getSaveButton().setEnabled(true);
+
+        // Button Panel
+        enableMovementButtons();
+    }
+
+    /**
+     * Enables the buttons used for question interaction.
+     */
+    protected static void enableQuestionButtons() {
+        // Question Panel
+        var currentPanel = QuestionPanel.getInstance();
+        currentPanel.getAButton().setEnabled(true);
+        currentPanel.getBButton().setEnabled(true);
+        currentPanel.getCButton().setEnabled(true);
+        currentPanel.getDButton().setEnabled(true);
+    }
+
+    /**
+     * Enables arrow buttons that allow users to move in the environment.
+     */
+    private static void enableMovementButtons() {
+        var currentPanel = ButtonPanel.getInstance();
+        currentPanel.getUpButton().setEnabled(true);
+        currentPanel.getRightButton().setEnabled(true);
+        currentPanel.getDownButton().setEnabled(true);
+        currentPanel.getLeftButton().setEnabled(true);
+    }
+}
