bf127dc0402d1cbdf836de6e8fc8bf0e2d4bd18a
slogansUW <83303380+slogansUW@users.noreply.github.com>
2021-06-01 17:56:31 -0700

    Created HelpButtonListener.java class

---
+++ src/controller/listener/HelpButtonListener.java
@@ -0,0 +1,28 @@
+package controller.listener;
+
+import java.awt.event.ActionEvent;
+
+/**
+ * Listener for HelpButton.
+ * @author Sean Lofan
+ * @version 1
+ */
+public class HelpButtonListener extends GameListener {
+
+    /**
+     * Used for the help button.
+     */
+    public HelpButtonListener() {
+        super();
+    }
+
+    /**
+     * Invoked when an action occurs.
+     *
+     * @param e the event to be processed
+     */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
